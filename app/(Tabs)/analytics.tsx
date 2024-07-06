034fd371d0e8d740eaf743323bdded0b22f59021
xyvie lyons <xyvielyons@gmail.com>
2024-07-06 03:49:40 +0300

    analytics page ui

--- app/(Tabs)/analytics.tsx
+++ app/(Tabs)/analytics.tsx
@@ -5,6 +5,11 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const Analytics = () => {
   return (
     <SafeAreaView className='bg-primarycolor h-full'>
+      <View className='bg-white justify-center items-center p-4'>
+        <Text className='text-[36px] font-rbold text-coolGray80'>Number of uploaded Journals</Text>
+        <Text className='text-[64px] font-rbold text-blue-500'>70</Text>
+
+      </View>
 
     </SafeAreaView>
   )
