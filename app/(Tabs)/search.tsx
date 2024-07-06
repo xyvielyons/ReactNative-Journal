e3dea52730d0aeb4ecefdcee2addc95a92f55e8b
xyvie lyons <xyvielyons@gmail.com>
2024-07-06 03:45:10 +0300

    search ui implementation

--- app/(Tabs)/search.tsx
+++ app/(Tabs)/search.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text,FlatList,ScrollView } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-
+import SearchBar from '@/components/searchbar'
+import Journals from '@/components/Journals'
+import { SearchIcon } from '@gluestack-ui/themed'
 const Search = () => {
   return (
     <SafeAreaView className='bg-primarycolor h-full'>
+     
+      
 
+        <View className='p-[16px]'>
+          <SearchBar
+          placeholder='search for a journal....'
+          ></SearchBar>
+
+        </View>
+
+        <View>
+          
+              <View className='flex-row justify-center items-center'>
+                <Text className='text-[20px] font-rmedium text-coolGray60 p-2'>Displaying results for:</Text>
+                <Text className='text-[24px] font-bold text-coolGray80 p-2'>A journey</Text>
+              </View>
+          <ScrollView className='h-full'>
+              
+              <Journals></Journals>
+
+          </ScrollView>
+
+          
+           
+
+           
+
+
+        </View>
+
+
+    
     </SafeAreaView>
   )
 }
